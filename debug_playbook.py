33b963fbb95d0a50ca99a21d378aa695296801bf
pipelinedave <kingovi@gmail.com>
2023-04-15 17:23:38 +0000

    added debug python script and vscode launch config, updated .gitignore

---
+++ debug_playbook.py
@@ -0,0 +1,14 @@
+import ansible_runner
+import os
+
+playbook_file = os.path.join(os.path.dirname(__file__), "playbook.yml")
+inventory_file = os.path.join(os.path.dirname(__file__), "inventory", "dawg-swarm.yml")
+
+runner = ansible_runner.run(
+    private_data_dir=os.path.dirname(__file__),
+    playbook=playbook_file,
+    inventory=inventory_file,
+    cmdline="--extra-vars '{\"var1\": \"value1\", \"var2\": \"value2\"}'",
+)
+
+print(runner.stats)
